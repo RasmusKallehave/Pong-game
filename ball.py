b71509dee5fbcbba44ce70e46c42b2ba81755e91
RasmusKallehave <rasmus.kallehave@hotmail.com>
2025-06-17 23:46:01 +0200

    ball.py speed up update

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 class Ball:
     def __init__(self, speed = 5):
@@ -10,12 +11,15 @@ class Ball:
         self._ball.goto(0, 0)
         self._dx = speed
         self._dy = speed
+        self._start_time = time.time()
+        self._speedups_done = 0
 
     def move(self):
         new_x = self._ball.xcor() + self._dx
         new_y = self._ball.ycor() + self._dy
         self._ball.setx(new_x)
         self._ball.sety(new_y)
+        self.maybe_speed_up()
 
     def bounce_y(self):
         self._dy *= -1
@@ -25,6 +29,11 @@ class Ball:
 
     def reset_position(self):
         self._ball.goto(0, 0)
+        self.reset_speed_timer()
+
+    def reset_speed_timer(self):
+        self._start_time = time.time()
+        self._speedups_done = 0
 
     def ycor(self):
         return self._ball.ycor()
@@ -41,3 +50,12 @@ class Ball:
     def turtle(self):
         return self._ball
     
+    def maybe_speed_up(self):
+        current_time = time.time()
+        elapsed = current_time - self._start_time
+        print(f"[DEBUG] Elapsed: {elapsed:.2f} sec, Speedups done: {self._speedups_done}")
+        if elapsed >= (self._speedups_done + 1) * 5:
+            self._dx *= 1.3
+            self._dy *= 1.3
+            self._speedups_done += 1
+            print(f"[SPEED UP] New dx: {self._dx:.2f}, dy: {self._dy:.2f}")
